a92b249e8baeec8ed36a73c00a5e62a1ee9b8762
hilla-m <hilla.manbar@gmail.com>
2021-05-09 18:48:05 +0300

    adding user name to navbar

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -1,13 +1,15 @@
-export default class UserModel {
-    constructor (plainUser){
-        this.id= plainUser.id,
-        this.fullName= plainUser.fullName,
-        this.email= plainUser.email,
-        this.password= plainUser.password,
-        this.playlists= plainUser.playlists,
-        this.albums= plainUser.albums,
-        this.artists= plainUser.artists
+class UserModel {
+    constructor(plainUser) {
+        this.id= plainUser.id;
+        this.name= plainUser.name;
+        this.email= plainUser.email;
+        this.password= plainUser.password;
+        this.playlists= plainUser.playlists;
+        this.albums= plainUser.albums;
+        this.artists= plainUser.artists;
     }
 
 }
+
+export default UserModel;
 
