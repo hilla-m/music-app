4288f6da39b658892b400bbde31c1e92bf76bdf2
hilla-m <hilla.manbar@gmail.com>
2021-05-09 10:27:43 +0300

    navbar fixes

--- src/components/MyMusicNavbar/MyMusicNavbar.js
+++ src/components/MyMusicNavbar/MyMusicNavbar.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
+import '../MyMusicNavbar/MyMusicNavbar.css'
 
 function MyMusicNavbar(props) {
     return (
-        <Navbar bg="dark" variant="dark" className="navbar">
+        <Navbar variant="dark" className="navbar navbar-bg" expand="lg">
             <Navbar.Brand href="#home">My Music
-            {/* <img src="music-logo-png.png" width="50" className="logo"/> */}
+            {/* <img src="music-logo.jpg" width="40" className="logo"/> */}
             </Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#all-playlists">Playlists</Nav.Link>
-                <Nav.Link href="#albums">Albums</Nav.Link>
-                <Nav.Link href="#artists">Artists</Nav.Link>
-            </Nav>
-            <Nav className="ml-auto">
-                <Nav.Link href="#login">Login</Nav.Link>
-                <Nav.Link href="#signup">Signup</Nav.Link>
-                <Nav.Link href="#">Logout</Nav.Link>
-            </Nav>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="nav-display">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#all-playlists">Playlists</Nav.Link>
+                    <Nav.Link href="#albums">Albums</Nav.Link>
+                    <Nav.Link href="#artists">Artists</Nav.Link>
+                </Nav>
+                <Nav className="ml-auto">
+                    <Nav.Link href="#login">Login</Nav.Link>
+                    <Nav.Link href="#signup">Signup</Nav.Link>
+                    <Nav.Link href="#">Logout</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
 }
