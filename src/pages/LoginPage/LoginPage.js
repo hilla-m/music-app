e429806a3c4e5dbdc27ffaab50ed17a1728fc682
hilla-m <hilla.manbar@gmail.com>
2021-05-09 18:07:26 +0300

    LoginPage controlled component

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form , Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './LoginPage.css';
 
 function LoginPage(props) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword]= useState("");
+    
+    function login(e){
+        e.preventDefault();
+    }
+
     return (
         <div className="p-login">
             <h1>Login to My Music</h1>
-            <Form>
+            <Form onSubmit={login}>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address:</Form.Label>
-                    <Form.Control type="email" placeholder="Email" />
+                    <Form.Control type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}/>
                 </Form.Group>
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password:</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
                 </Form.Group>
-                <button type="button" className="btn">Login</button>
+                <button type="submit" className="btn">Login</button>
             </Form>
             <p>Don't have an account yet ?</p>
             <button type="button" className="btn"><Link to="/signup" className="signup-link">Signup for free</Link></button>
