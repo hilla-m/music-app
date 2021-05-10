c8fb5bfbb26903995d12f1077bb384c895445ce4
hilla-m <hilla.manbar@gmail.com>
2021-05-10 12:17:35 +0300

    adding filter to homepage +  renddering AlbumCard

---
+++ src/components/AlbumCard/AlbumCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AlbumCard.css';
+
+function AlbumCard(props) {
+    return (
+        <div>
+            AlbumCard
+        </div>
+    );
+}
+
+export default AlbumCard;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
+import { Form, Row, Spinner } from 'react-bootstrap';
 import { Redirect } from 'react-router';
+import './HomePage.css';
+import AlbumCard from '../../components/AlbumCard/AlbumCard';
 
-function HomePage({activeUser}) {
-    if(!activeUser){
-        return <Redirect to="/"/>
+function HomePage({ activeUser, albums, artists }) {
+    const[filterText,setFilterText]=useState("");
+    
+    if (!activeUser) {
+        return <Redirect to="/" />
     }
+
+    const filteredAlbums = albums.filter(album => album.title.toLowerCase().includes(filterText.toLowerCase()));
+    // || album.artistId  .includes(filterText.toLowerCase()))
+
+    const albumsCards = filteredAlbums.map(album => <AlbumCard album={album} />);
+
     return (
-        <div>
-            HomePage
+        <div className="p-home">
+            <h1>Find your music</h1>
+            <Form>
+                <Form.Control type="text" className="filter" placeholder="Search album" value={filterText} onChange={e => setFilterText(e.target.value)} />
+            </Form>
+
+            {albums ?
+                    <Row className="cards-div" xs={1} md={2} lg={5}>
+                        {albumsCards}
+                    </Row> :
+                    <Spinner animation="border" />
+                }
         </div>
     );
 }
