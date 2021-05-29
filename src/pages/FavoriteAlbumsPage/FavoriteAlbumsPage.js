8ae53887e76c8e7ad6397b58d836771b76814b07
hilla-m <hilla.manbar@gmail.com>
2021-05-29 17:25:25 +0300

    adding h1 + css to FavoriteAlbumsPage

--- src/pages/FavoriteAlbumsPage/FavoriteAlbumsPage.js
+++ src/pages/FavoriteAlbumsPage/FavoriteAlbumsPage.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { FaHeart } from 'react-icons/fa';
 import { Redirect } from 'react-router';
 import AlbumCard from '../../components/AlbumCard/AlbumCard';
+import './FavoriteAlbumsPage.css';
 
 function FavoriteAlbumsPage({ activeUser, albums, artists }) {
     if (!activeUser) {
         return <Redirect to="/" />
     }
-    
+
     const favAlbums = albums.filter(album => activeUser.albums.indexOf(album.id) !== -1);
 
     return (
-        <div className="fav-albums">
+        <div className="p-fav-albums">
+            <h1>My Favorite Albums</h1>
             {favAlbums.length > 0 ?
                 <Row>
-                    {favAlbums.map(album => <Col sm={6} md={2} > <AlbumCard album={album} artist={artists.find(artist => artist.id === album.artistId)} /></Col>)}
+                    {favAlbums.map(album => <Col sm={6} md={2} > <AlbumCard album={album} artist={artists.find(artist => artist.id === album.artistId)} /> </Col>)}
+                 
                 </Row> :
                 null
             }
