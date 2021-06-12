969aa362029494c738cd70195c7c7ccf7ee0812a
hilla-m <hilla.manbar@gmail.com>
2021-06-12 15:04:07 +0300

    adding ArtistCard

---
+++ src/components/ArtistCard/ArtistCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './ArtistCard.css';
+
+function ArtistCard(artist) {
+
+    const artistIndex = artist.id - 1;
+
+    return (
+        <Card className="artist-card">
+            <Card.Img className="artist-img" variant="top" src={artist.image} />
+            <Card.Body>
+                <Card.Title>
+                    <Link to={"/artist/" + artistIndex} className="artist-link"> {artist.name}</Link>
+                </Card.Title>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default ArtistCard;
